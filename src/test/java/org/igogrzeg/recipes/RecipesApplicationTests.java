623688168f04b7eb045b24535976529d5568b0ce
IgorKawczynski <kluseczkibabuni94@gmail.com>
2022-08-22 11:29:52 +0200

    Uploading project...

---
+++ src/test/java/org/igogrzeg/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.igogrzeg.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
