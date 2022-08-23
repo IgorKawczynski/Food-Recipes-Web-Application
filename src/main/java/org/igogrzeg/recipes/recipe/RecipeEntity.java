790206f4ad737c22dc6400c9675bd5a64eced954
GKrych <hazzidy125@gmail.com>
2022-08-23 12:30:59 +0200

    Added entity relations

--- src/main/java/org/igogrzeg/recipes/recipe/RecipeEntity.java
+++ src/main/java/org/igogrzeg/recipes/recipe/RecipeEntity.java
@@ -2,13 +2,18 @@ package org.igogrzeg.recipes.recipe;
 
 import lombok.NoArgsConstructor;
 import org.igogrzeg.recipes.basic.BasicEntity;
+import org.igogrzeg.recipes.user.UserEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @NoArgsConstructor
 @Entity
+@Table(name = "recipes")
 public class RecipeEntity extends BasicEntity {
 
-
+    @ManyToOne
+    private UserEntity user;
 
 }
