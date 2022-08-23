1ca96f7f0abfe4c9b3528f5f40f88544ca7b2995
GKrych <hazzidy125@gmail.com>
2022-08-23 09:47:49 +0200

    Added basic entities code

---
+++ src/main/java/org/igogrzeg/recipes/recipe/RecipeEntity.java
@@ -0,0 +1,10 @@
+package org.igogrzeg.recipes.recipe;
+
+import org.igogrzeg.recipes.basic.BasicEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class RecipeEntity extends BasicEntity {
+
+}
---
+++ src/main/java/org/igogrzeg/recipes/user/UserEntity.java
@@ -0,0 +1,13 @@
+package org.igogrzeg.recipes.user;
+
+import org.igogrzeg.recipes.basic.BasicEntity;
+import org.igogrzeg.recipes.recipe.RecipeEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class UserEntity extends BasicEntity {
+
+}
